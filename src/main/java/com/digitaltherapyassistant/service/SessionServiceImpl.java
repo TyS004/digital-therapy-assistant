ec5edb2933688d54ed6d2716f9c5ff05a7cb4db0
TyS004 <76879612+TyS004@users.noreply.github.com>
2026-04-08 04:46:32 -0700

    DTO changes

--- src/main/java/com/digitaltherapyassistant/service/SessionServiceImpl.java
+++ src/main/java/com/digitaltherapyassistant/service/SessionServiceImpl.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Service;
 import com.digitaltherapyassistant.dto.response.session.ActiveSession;
 import com.digitaltherapyassistant.dto.response.session.ChatResponse;
 import com.digitaltherapyassistant.dto.response.session.SessionDetail;
+import com.digitaltherapyassistant.dto.response.session.SessionDto;
 import com.digitaltherapyassistant.dto.response.session.SessionHistoryEntry;
 import com.digitaltherapyassistant.dto.response.session.SessionModuleDto;
 import com.digitaltherapyassistant.dto.response.session.SessionSummary;
@@ -23,6 +24,7 @@ import com.digitaltherapyassistant.entity.UserSession;
 import com.digitaltherapyassistant.exception.DigitalTherapyException;
 import com.digitaltherapyassistant.repository.CbtSessionRepository;
 import com.digitaltherapyassistant.repository.ChatMessageRepository;
+import com.digitaltherapyassistant.repository.SessionModuleRepository;
 import com.digitaltherapyassistant.repository.UserRepository;
 import com.digitaltherapyassistant.repository.UserSessionRepository;
 
@@ -32,27 +34,44 @@ public class SessionServiceImpl implements SessionService{
     private final UserRepository userRepository;
     private final UserSessionRepository userSessionRepository;
     private final ChatMessageRepository chatMessageRepository;
+    private final SessionModuleRepository sessionModuleRepository;
 
     public SessionServiceImpl(CbtSessionRepository cbtSessionRepository,
                                 UserRepository userRepository,
                                 UserSessionRepository userSessionRepository,
-                                ChatMessageRepository chatMessageRepository
+                                ChatMessageRepository chatMessageRepository,
+                                SessionModuleRepository sessionModuleRepository
     ){
         this.cbtSessionRepository = cbtSessionRepository;
         this.userRepository = userRepository;
         this.userSessionRepository = userSessionRepository;
         this.chatMessageRepository = chatMessageRepository;
+        this.sessionModuleRepository = sessionModuleRepository;
     }
 
     public List<SessionModuleDto> getSessionLibrary(UUID userId){
+        List<SessionModule> modules = sessionModuleRepository.findAll();
         List<SessionModuleDto> response = new ArrayList<>();
-        List<CbtSession> sessions = cbtSessionRepository.findAll();
-
-        for(CbtSession session : sessions){
-            SessionModule sessionModule = session.getModule();
+        
+        for (SessionModule module : modules) {
             SessionModuleDto moduleDto = new SessionModuleDto();
-            moduleDto.setModule(sessionModule);
-            moduleDto.setMessage("Set Module");
+            moduleDto.setId(module.getId());
+
+            List<SessionDto> sessionDtos = new ArrayList<>();
+            for (CbtSession session : module.getCbtSessions()) {
+                SessionDto sessionDto = new SessionDto();
+
+                sessionDto.setId(session.getId());
+                sessionDto.setTitle(session.getTitle());
+                sessionDto.setDescription(session.getDescription());
+                sessionDto.setDurationMinutes(session.getDurationMinutes());
+                sessionDto.setObjectives(session.getObjectives());
+                sessionDto.setModalities(session.getModalities());
+                sessionDto.setOrderIndex(session.getOrderIndex());
+                sessionDtos.add(sessionDto);
+            }
+            moduleDto.setSessions(sessionDtos);
+            moduleDto.setMessage("Retreived Session Data for Module");
             response.add(moduleDto);
         }
         return response;
@@ -63,7 +82,13 @@ public class SessionServiceImpl implements SessionService{
         CbtSession session = cbtSessionRepository.findById(sessionId)
             .orElseThrow(() -> new DigitalTherapyException("Session Not Found"));
 
-        response.setSession(session);
+        response.setId(sessionId);
+        response.setDescription(session.getDescription());
+        response.setDurationMinutes(session.getDurationMinutes());
+        response.setModalities(session.getModalities());
+        response.setObjectives(session.getObjectives());
+        response.setOrderIndex(session.getOrderIndex());
+        response.setTitle(session.getTitle());
         response.setMessage("Retireved Session Details");
         return response;
     }
@@ -86,9 +111,14 @@ public class SessionServiceImpl implements SessionService{
         userSession.setStatus(Status.IN_PROGRESS);
         userSession.setStartedAt(LocalDateTime.now());
         userSession.setChatMessages(new ArrayList<>());
+        userSession.setMoodBefore(5);
         userSessionRepository.save(userSession);
 
-        response.setSession(userSession);
+        response.setMoodBefore(userSession.getMoodBefore());
+        response.setStartedAt(userSession.getStartedAt());
+        response.setStatus(userSession.getStatus());
+        response.setSessionId(sessionId);
+        response.setTitle(cbtSession.getTitle());
         response.setMessage("Session Started");
         return response;
     }
@@ -112,7 +142,10 @@ public class SessionServiceImpl implements SessionService{
         List<ChatMessage> chatMessages = chatMessageRepository.findAllByUserSession(userSession);
         chatMessages.add(chatMessage);
 
-        response.setChatMessage(chatMessage);
+        response.setSessionId(sessionId);
+        response.setTimestamp(chatMessage.getTimestamp());
+        //TODO CHANGE TO AI SERVICE 
+        response.setAssistantMessage(null);
         response.setMessage("Message Set");
         return response;
     }
@@ -130,7 +163,12 @@ public class SessionServiceImpl implements SessionService{
         userSession.setStatus(Status.COMPLETED);
         userSessionRepository.delete(userSession);
 
-        response.setSession(session);
+        response.setStartedAt(userSession.getStartedAt());
+        response.setEndedAt(userSession.getEndedAt());
+        response.setMoodBefore(userSession.getMoodBefore());
+        response.setMoodAfter(userSession.getMoodAfter());
+        response.setSessionId(sessionId);
+        response.setStatus(userSession.getStatus());
         response.setReason(reason); 
         response.setMessage("Session Ended");
 
@@ -148,7 +186,13 @@ public class SessionServiceImpl implements SessionService{
 
         for(UserSession session : userSessions){
             SessionHistoryEntry sessionHistory = new SessionHistoryEntry();
-            sessionHistory.setUserSession(session);
+
+            sessionHistory.setStartedAt(session.getStartedAt());
+            sessionHistory.setEndedAt(session.getEndedAt());
+            sessionHistory.setMoodBefore(session.getMoodBefore());
+            sessionHistory.setMoodAfter(session.getMoodAfter());
+            sessionHistory.setSessionId(session.getId());
+            sessionHistory.setStatus(session.getStatus());
             sessionHistory.setMessage("Set Session " + session.getId());
             response.add(sessionHistory);
         }
